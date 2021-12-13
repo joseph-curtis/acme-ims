1b5a24468abdd85f3696e6ac8a2e3b78771d96db
Joseph Curtis <jcur175@wgu.edu>
2021-12-13 13:15:11 -0800

    added module-info

---
+++ module-info.java
@@ -0,0 +1,8 @@
+/**
+ * Inventory Management System using JavaFX for a small manufacturing organization
+ * <p>If you use this software, please cite it as indicated in the CITATION.cff file.</p>
+ */
+module src {
+    requires javafx.fxml;
+    requires javafx.controls;
+}
