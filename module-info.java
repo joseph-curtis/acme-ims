14986c2d8b1e27434db2a9122e724b09f67faebd
Joseph Curtis <jcur175@wgu.edu>
2023-03-12 17:56:25 -0700

    added dependencies

--- module-info.java
+++ module-info.java
@@ -3,6 +3,8 @@
  * <p>If you use this software, please cite it as indicated in the CITATION.cff file.</p>
  */
 module src {
+    requires transitive javafx.base;
     requires javafx.fxml;
     requires javafx.controls;
+    requires javafx.graphics;
 }
